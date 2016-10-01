77845e76732a2fde550cfaacd3e6b872468d3393
Charles Valerio Howlader <charles.vh_14@live.com>
2016-10-01 06:48:33 +0600

    files modified

--- task3.php
+++ task3.php
@@ -1,10 +1,16 @@
 <?php
 if (!empty($_POST['your_name'])){
-    echo "Your Name".$_POST['your_name'];
+    echo "Your Name: ".$_POST['your_name'];
+}
+echo "<br>";
+if (!empty($_POST['your_Roll'])){
+    echo "Your Roll: " .$_POST['your_Roll'];
+}
+echo "<br>";
+if (!empty($_POST['your_Phone'])){
+    echo "Your Phone: " .$_POST['your_Phone'];
 }
 else{
     echo "You didn’t enter your name";
-
     }
-}
 ?>
